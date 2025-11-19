37c796287d3b4eeb83595c23295edcc1539ec9e4
Kirill Belikov <angelodelamorte@yandex.ru>
2025-11-19 09:11:28 +0300

    Fixing issues - removing locators, moving login to conftest. Add requirements.txt

--- conftest.py
+++ conftest.py
@@ -4,6 +4,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Locators for login
+USERNAME_FIELD = (By.ID, "user-name")
+PASSWORD_FIELD = (By.ID, "password")
+LOGIN_BUTTON = (By.ID, "login-button")
+INVENTORY_ITEMS = (By.CSS_SELECTOR, "[data-test='inventory-container']")
+
 
 @pytest.fixture(scope="function")
 def driver(request):
@@ -24,6 +34,21 @@ def driver(request):
 
     yield driver
     driver.quit()
+
+@pytest.fixture(scope="function")
+def logged_in(driver):
+    driver.get("https://www.saucedemo.com")
+
+    # Login
+    driver.find_element(*USERNAME_FIELD).send_keys("performance_glitch_user")
+    driver.find_element(*PASSWORD_FIELD).send_keys("secret_sauce")
+    driver.find_element(*LOGIN_BUTTON).click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(INVENTORY_ITEMS)
+    )
+
+    return driver
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests: chrome or firefox")
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,56 +1,45 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 
-def test_open_page(driver):
-    driver.get("https://www.saucedemo.com")
+# Locators for test
+PRODUCT_CARDS = (By.CLASS_NAME, "inventory_item")
+ADD_TO_CART_SAUCE_LABS_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
+ADD_TO_CART_SAUCE_LABS_ONESIE = (By.ID, "add-to-cart-sauce-labs-onesie")
+ADD_TO_CART_SAUCE_LABS_FLEECE_JACKET = (By.ID, "add-to-cart-sauce-labs-fleece-jacket")
+SHOPPING_CART_CONTAINER = (By.ID, "shopping_cart_container")
+CART_BADGE = (By.CSS_SELECTOR, "[data-test='shopping-cart-badge']")
+INVENTORY_ITEMS_NAMES = (By.CSS_SELECTOR, "[data-test='inventory-item-name']")
+
+def add_product_to_cart(driver, locator, timeout=10):
+    WebDriverWait(driver, timeout).until(
+        EC.element_to_be_clickable(locator)
+    ).click()
+
+def test_open_page(logged_in):
+    driver = logged_in
     assert "Swag Labs" in driver.title
 
-    # Login
-    username_field = driver.find_element(By.ID, "user-name")
-    password_field = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "login-button")
-
-    username_field.send_keys("standard_user")
-    password_field.send_keys("secret_sauce")
-    login_button.click()
-
     #Products
-    product_cards = driver.find_elements(By.CLASS_NAME, "inventory_item")
+    product_cards = driver.find_elements(*PRODUCT_CARDS)
     assert len(product_cards) == 6, f"Expected 6 products, got {len(product_cards)}"
 
-def test_add_cart(driver):
-    driver.get("https://www.saucedemo.com")
-
-    # Login
-    username_field = driver.find_element(By.ID, "user-name")
-    password_field = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "login-button")
-
-    username_field.send_keys("performance_glitch_user")
-    password_field.send_keys("secret_sauce")
-    login_button.click()
+def test_add_cart(logged_in):
+    driver = logged_in
 
     # Add to cart
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))
-    ).click()
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-onesie"))
-    ).click()
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-fleece-jacket"))
-    ).click()
+    add_product_to_cart(driver, ADD_TO_CART_SAUCE_LABS_BACKPACK)
+    add_product_to_cart(driver, ADD_TO_CART_SAUCE_LABS_ONESIE)
+    add_product_to_cart(driver, ADD_TO_CART_SAUCE_LABS_FLEECE_JACKET)
 
     # Shopping cart container
-    shopping_cart_button = driver.find_element(By.ID, "shopping_cart_container").click()
+    driver.find_element(*SHOPPING_CART_CONTAINER).click()
 
-    cart_badge = driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-badge']")
+    cart_badge = driver.find_element(*CART_BADGE)
     assert cart_badge.text == "3"
 
-    inventory_item_names = driver.find_elements(By.CSS_SELECTOR, "[data-test='inventory-item-name']")
+    inventory_item_names = driver.find_elements(*INVENTORY_ITEMS_NAMES)
     assert inventory_item_names[0].text == "Sauce Labs Backpack"
     assert inventory_item_names[1].text == "Sauce Labs Onesie"
     assert inventory_item_names[2].text == "Sauce Labs Fleece Jacket"
