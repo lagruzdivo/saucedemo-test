d7fc53d5a28e43a34b2e086e4e817c18393e72d6
Kirill Belikov <angelodelamorte@yandex.ru>
2025-11-22 09:14:57 +0300

    Add test for checkout complete

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,6 +10,12 @@ ADD_TO_CART_SAUCE_LABS_FLEECE_JACKET = (By.ID, "add-to-cart-sauce-labs-fleece-ja
 SHOPPING_CART_CONTAINER = (By.ID, "shopping_cart_container")
 CART_BADGE = (By.CSS_SELECTOR, "[data-test='shopping-cart-badge']")
 INVENTORY_ITEMS_NAMES = (By.CSS_SELECTOR, "[data-test='inventory-item-name']")
+CHECKOUT_BUTTON = (By.ID, "checkout")
+FIRST_NAME = (By.ID, "first-name")
+LAST_NAME = (By.ID, "last-name")
+POSTAL_CODE = (By.ID, "postal-code")
+CONTINUE_BUTTON = (By.ID, "continue")
+FINISH_BUTTON = (By.ID, "finish")
 
 def add_product_to_cart(driver, locator, timeout=10):
     WebDriverWait(driver, timeout).until(
@@ -43,3 +49,17 @@ def test_add_cart(logged_in):
     assert inventory_item_names[0].text == "Sauce Labs Backpack"
     assert inventory_item_names[1].text == "Sauce Labs Onesie"
     assert inventory_item_names[2].text == "Sauce Labs Fleece Jacket"
+
+    driver.find_element(*CHECKOUT_BUTTON).click()
+
+    # Checkout: Your information
+    driver.find_element(*FIRST_NAME).send_keys("John")
+    driver.find_element(*LAST_NAME).send_keys("Doe")
+    driver.find_element(*POSTAL_CODE).send_keys("123456")
+    driver.find_element(*CONTINUE_BUTTON).click()
+
+    driver.find_element(*FINISH_BUTTON).click()
+
+    checkout_complete = driver.find_element(By.CSS_SELECTOR, "[data-test='complete-header']")
+    assert checkout_complete.text == "Thank you for your order"
+
